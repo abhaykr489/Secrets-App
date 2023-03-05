383373fe2b8c223e5e90cee6e7d4bf200da4218b
abhaykr489 <86096153+abhaykr489@users.noreply.github.com>
2023-03-05 18:19:08 +0530

    set port address

--- app.js
+++ app.js
@@ -188,8 +188,8 @@ app.post("/login", function(req, res){
 
 
 
+const PORT = process.env.PORT || 3000
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(PORT, function() {
+  console.log("Server started sucessfully.");
 });
